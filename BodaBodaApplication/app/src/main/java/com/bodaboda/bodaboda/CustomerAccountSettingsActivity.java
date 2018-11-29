346cfa910a9edceb485646b3b67dd3a9fb181c3d
Jonathan <jmr16009@student.mdh.se>
2018-11-29 19:27:37 +0100

    Adding the Android Studio BodaBoda Project!

---
+++ BodaBodaApplication/app/src/main/java/com/bodaboda/bodaboda/CustomerAccountSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.bodaboda.bodaboda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerAccountSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_account_settings);
+    }
+}
