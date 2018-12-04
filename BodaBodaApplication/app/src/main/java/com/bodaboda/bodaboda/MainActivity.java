19c610814f8d765274401e09e789748f5385c174
Jonathan <jmr16009@student.mdh.se>
2018-12-04 12:54:38 +0100

    Implementing communication from the application with retrofit

--- BodaBodaApplication/app/src/main/java/com/bodaboda/bodaboda/MainActivity.java
+++ BodaBodaApplication/app/src/main/java/com/bodaboda/bodaboda/MainActivity.java
@@ -5,6 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,10 +38,44 @@ public class MainActivity extends AppCompatActivity {
 
     private void initLoginButton()
     {
+        final EditText username = (EditText) findViewById(R.id.main_enter_username_editText);
+        final EditText password = (EditText) findViewById(R.id.main_enter_password_editText);
         Button loginButton = (Button)findViewById(R.id.main_login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //Check if the info in every field is okey before sending
+                /*if(notOkey){
+                    return;
+                }*/
+
+                //Send fields to server for check
+                Call<ResponseBody> call = BodaBodaClient
+                        .getInstance()
+                        .getApi()
+                        .loginRequest(
+                                username.toString(),
+                                password.toString()
+                        );
+
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        /*if(response.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Server is up and got the message!", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Server is up but return error!", Toast.LENGTH_LONG).show();
+                        }*/
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        //Toast.makeText(MainActivity.this, "Network failure", Toast.LENGTH_LONG).show();
+                    }
+                });
+
                 //Need to verify first and choose driver or customer
                 Intent loginIntent = new Intent(MainActivity.this, CustomerMainActivity.class);
                 MainActivity.this.startActivity(loginIntent);
