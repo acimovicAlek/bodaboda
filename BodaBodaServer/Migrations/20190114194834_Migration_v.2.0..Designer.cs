b91433c9c70d8f1de7b6bafa0e81029e57c64a05
acimovicAlek <acimovic.alek@gmail.com>
2019-01-14 20:48:56 +0100

    Migration 2.0

--- BodaBodaServer/Migrations/20190114194834_Migration_v.2.0..Designer.cs
+++ BodaBodaServer/Migrations/20190114194834_Migration_v.2.0..Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BodaBodaServer.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BodaBodaServer.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20190114194834_Migration_v.2.0.")]
+    partial class Migration_v20
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BodaBodaServer/Migrations/20190114194834_Migration_v.2.0..cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BodaBodaServer.Migrations
+{
+    public partial class Migration_v20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Users_UserId",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Locations");
+
+            migrationBuilder.AddColumn<long>(
+                name: "LocationId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LocationId",
+                table: "Users",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Locations",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Users_UserId",
+                table: "Locations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
